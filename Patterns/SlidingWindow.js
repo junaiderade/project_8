e43e949d918c147755b9d30d97fe2e9530671d9c
Mohamed <junaid98m@gmail.com>
2022-10-31 14:55:58 -0400

    did some qs

--- Patterns/SlidingWindow.js
+++ Patterns/SlidingWindow.js
@@ -1,20 +1,3 @@
-/*
-Time: O(n)
-Space: O(1)
-Date: 10/31/2022
-Desc: Given an integer array nums, find the subarray which has the largest sum and return its sum.
-    Example 1:
-    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-    Output: 6
-    Explanation: [4,-1,2,1] has the largest sum = 6.
-    Example 2:
-    Input: nums = [1]
-    Output: 1
-    Example 3:
-    Input: nums = [5,4,-1,7,8]
-    Output: 23
-*/
-
 var maxSubArray = function(nums) {
     let global_max = nums[0];
     let local_max = nums[0];
@@ -25,6 +8,53 @@ var maxSubArray = function(nums) {
         global_max=Math.max(local_max,global_max);
     }
     
-    return global_max
+    return global_max;
 };
+
+/*
+Time: O(n)
+Space: O(1)
+Date: 10/31/2022
+Desc: Given an integer array nums, find the subarray which has the largest sum and return its sum.
+Note: Right on first try
+---------------------------------------------------------------------------------------------------------------------
+*/
+
+var minSubArrayLen = function(s, nums) {
+    let start = 0;
+    let end = 0;
+    let sum = 0;
+    let minLen = nums.length;
+    let found = false;
+
+    while(end < nums.length+1){
+        if(sum < s){
+            sum+=nums[end];
+            end++;
+        }else{
+            found = true
+            sum-=nums[start];
+            minLen = Math.min(end-start,minLen);
+            start++
+        }
+    }
+    
+    if(found){
+        return minLen;
+    }
+    
+    return 0;
+};
+
+/*
+Time: O(n)
+    - this is because each number can be visited a maximum of 2 times 
+Space: O(1)
+Date: 10/31/2022
+Desc: Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+Note: Needed to look at solution
+---------------------------------------------------------------------------------------------------------------------
+
+*/
+
 
